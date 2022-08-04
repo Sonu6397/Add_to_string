f8ddc001ca17a1d934db427ec02215e0e9607b8b
admin <admin@CHETUIWK1860>
2022-08-04 15:25:15 +0530

    Add project files.

---
+++ Add_to_string/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Add_to_string
+{
+   public class student
+    {
+        public void Add()
+        {
+            Console.WriteLine("enter first string");
+            string s1 = (Console.ReadLine());
+            Console.WriteLine("enter second string");
+            string s2 = (Console.ReadLine());
+
+            Console.WriteLine(string.Concat(s1,s2));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            s.Add();
+            Console.ReadLine();
+        }
+    }
+}
